4031e8e93431e4c3440a44116267c0b489eab1a6
wangrunchang <wrc1028@163.com>
2022-02-21 19:38:23 +0800

    2/21 19:40 Final Push

--- Assets/RenderFeatures/CustomRendererFeature.cs
+++ Assets/RenderFeatures/CustomRendererFeature.cs
@@ -8,7 +8,22 @@ public class CustomRendererFeature : ScriptableRendererFeature
     // 实际负责渲染逻辑工作的类
     class CustomRenderPass : ScriptableRenderPass
     {
+        // This struct contains all the information required to create a RenderTexture.
+        // 一个描述如何创建RenderTexture的结构体
+        private RenderTextureDescriptor m_RTDescriptor;
+        // RenderTargetIdentifier和RenderTargetHandle都在CommandBuffer内对RenderTarget进行标识(描述)
+        // RenderTargetHandle只是一个包装类, 用于将着色器属性id映射到RenderTargetIdentifier.
+        private RenderTargetIdentifier m_RTIdentifier;
+        private RenderTargetHandle m_RTHandle;
+        // 可以通过当前这个方法重新定向渲染目标, 但是只在当前Pass生效
+        // 因为下一个运行的Pass只会从它所配置的地方获取属性以和设置渲染目标
+        // 当前参数由当前渲染流水线中的Renderer设置
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            
+        }
         // 渲染之前, Renderer调用此方法, 配置渲染目标及清除状态, 并创建临时渲染目标纹理
+        // 如果渲染过程未重写这个方法，则该渲染过程将渲染到激活状态下 Camera 的渲染目标
         public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
         {
         }
@@ -18,7 +33,7 @@ public class CustomRendererFeature : ScriptableRendererFeature
         {
         }
 
-        // 完成渲染相机后调用, 释放通过此过程创建的资源
+        // 用于释放通过此过程创建的分配资源。完成渲染相机后调用。就可以使用此回调释放此渲染过程创建的所有资源。
         public override void OnCameraCleanup(CommandBuffer cmd)
         {
         }
@@ -26,7 +41,9 @@ public class CustomRendererFeature : ScriptableRendererFeature
 
     CustomRenderPass m_ScriptablePass;
 
-    /// 初始化Feature的资源
+    /// <summary>
+    /// 初始化Pass资源
+    /// </summary>
     public override void Create()
     {
         m_ScriptablePass = new CustomRenderPass();
--- Assets/RenderFeatures/ZoomBlur/ZoomBlurRenderFeature.cs
+++ Assets/RenderFeatures/ZoomBlur/ZoomBlurRenderFeature.cs
@@ -9,18 +9,21 @@ public class ZoomBlurRenderFeature : ScriptableRendererFeature
     {
         // 显示在FrameDebugger的标签
         private static readonly string k_RenderTag = "Render ZoomBlur Effects";
-        // 获得当前用于后处理 Shader 的参数ID
-        private static readonly int MainTexId = Shader.PropertyToID("_MainTex");
-        private static readonly int TempTargetId = Shader.PropertyToID("_TempTargetZoomBlur");
+        // 为了提高效率，Unity为着色器属性的每个名称(例如，_MainTex或_Color)都分配一个唯一的整数，在整个游戏中保持不变。使用属性标识符比向所有物质属性函数传递字符串更有效。
+        // private static readonly int MainTexId = Shader.PropertyToID("_MainTex");
+        private RenderTargetHandle m_MainTexHandle = new RenderTargetHandle();
         private static readonly int FocusPowerId = Shader.PropertyToID("_FocusPower");
         private static readonly int FocusDetailId = Shader.PropertyToID("_FocusDetail");
         private static readonly int FocusScreenPositonId = Shader.PropertyToID("_FocusScreenPositon");
         private static readonly int ReferenceResolutionXId = Shader.PropertyToID("_ReferenceResolutionX");
-        // 成员变量
         private ZoomBlur zoomBlur;
         private Material zoomBlurMat;
-        private RenderTargetIdentifier currentTarget;
-
+        // 一个描述RT的结构体
+        private RenderTextureDescriptor m_TempTargetDescriptor;
+        // 用于创建渲染目标的结构体
+        private RenderTargetHandle m_TempTarget;
+        private RenderTargetHandle m_CurrentTarget;
+        private RenderTargetIdentifier a;
         public ZoomBlurRenderPass(RenderPassEvent evt)
         {
             renderPassEvent = evt;
@@ -37,8 +40,17 @@ public class ZoomBlurRenderFeature : ScriptableRendererFeature
         // 
         public void Setup(in RenderTargetIdentifier currentTarget)
         {
-            this.currentTarget = currentTarget;
+            m_CurrentTarget = new RenderTargetHandle(currentTarget);
         }
+        
+        private RenderTargetHandle m_CustomRenderTarget = new RenderTargetHandle();
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            // m_CustomRenderTarget.Init("_CustomRenderTarget");
+            // cmd.GetTemporaryRT(m_CustomRenderTarget.id, cameraTextureDescriptor, FilterMode.Bilinear);
+            // cmd.SetRenderTarget(m_CustomRenderTarget.Identifier());
+        }
+
         // public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
         // {
         // }
@@ -46,7 +58,7 @@ public class ZoomBlurRenderFeature : ScriptableRendererFeature
         /// <summary>
         /// 具体的后处理的执行规则
         /// </summary>
-        /// <param name="context">定义自定义渲染管道使用的状态和图形命令</param>
+        /// <param name="context">自定义渲染管道使用的状态和图形命令</param>
         /// <param name="renderingData">用于最终渲染的渲染数据汇总: 其中包括剔除后的结果、相机参数、灯光数据等等</param>
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
@@ -63,7 +75,7 @@ public class ZoomBlurRenderFeature : ScriptableRendererFeature
             var stack = VolumeManager.instance.stack;
             zoomBlur = stack.GetComponent<ZoomBlur>();
             if (zoomBlur == null || !zoomBlur.IsActive()) return;
-            // 2、从命令缓存池中获取一个gl命令缓存(CommandBuffer), 收集一系列GL指令, 用于执行
+            // 2、从命令缓存池中获取一个gl命令缓存(CommandBuffer), 用于收集一系列GL指令, 然后执行
             CommandBuffer zoomBlurBuffer = CommandBufferPool.Get(k_RenderTag);
             // 3、在命令缓存中构建渲染命令
             Render(zoomBlurBuffer, ref renderingData);
@@ -77,15 +89,14 @@ public class ZoomBlurRenderFeature : ScriptableRendererFeature
         {
             // 获得相机的渲染数据
             ref CameraData cameraData = ref renderingData.cameraData;
-            var source = currentTarget; // ???
-            int destination = TempTargetId; // ???
+            m_TempTarget.Init("_TempRenderTarget");
+            m_MainTexHandle.Init("_MainTex");
 
-            // 获得渲染相机的分辨率
-            int width = cameraData.camera.scaledPixelWidth;
-            int height = cameraData.camera.scaledPixelHeight;
+            // 获得渲染相机的基本参数
+            m_TempTargetDescriptor = renderingData.cameraData.cameraTargetDescriptor;
 
             // 对渲染材质的值进行设置
-            zoomBlurMat.SetFloat(FocusDetailId, zoomBlur.focusDetail.value);
+            zoomBlurMat.SetFloat(FocusPowerId, zoomBlur.focusPower.value);
             zoomBlurMat.SetInt(FocusDetailId, zoomBlur.focusDetail.value);
             zoomBlurMat.SetVector(FocusScreenPositonId, zoomBlur.focusScreenPositon.value);
             zoomBlurMat.SetInt(ReferenceResolutionXId, zoomBlur.referenceResolutionX.value);
@@ -93,17 +104,22 @@ public class ZoomBlurRenderFeature : ScriptableRendererFeature
             int shaderPass = 0;
 
             // 将当前Camera渲染的颜色结果, 当成_MainTex属性传入
-            buffer.SetGlobalTexture(MainTexId, source);
+            buffer.SetGlobalTexture(m_MainTexHandle.id, m_CurrentTarget.Identifier());
             // 在 CommandBuffer 中获得一张临时的RenderTexture
-            buffer.GetTemporaryRT(destination, width, height, 0, FilterMode.Point, RenderTextureFormat.Default);
+            buffer.GetTemporaryRT(m_TempTarget.id, m_TempTargetDescriptor, FilterMode.Point);
             // 因为当前渲染的结果要用于下一次渲染, 也就是说下一次渲染要从source指定的位置获取数据, 所以要以要将当次渲染结果输入到source指定的缓存地方
-            buffer.Blit(source, destination);
-            buffer.Blit(destination, source, zoomBlurMat, shaderPass);
-        }   
+            buffer.Blit(m_CurrentTarget.Identifier(), m_TempTarget.Identifier());
+            buffer.Blit(m_TempTarget.Identifier(), m_CurrentTarget.Identifier(), zoomBlurMat, shaderPass);
+        }
 
         // public override void OnCameraCleanup(CommandBuffer cmd)
         // {
         // }
+
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            cmd.ReleaseTemporaryRT(m_CustomRenderTarget.id);
+        }
     }
 
     public RenderPassEvent renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
@@ -117,7 +133,8 @@ public class ZoomBlurRenderFeature : ScriptableRendererFeature
     // 在 Renderer 中插入我们自定义的 ZoomBlurPass
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
-        // 初始化当前Pass, 将当前摄像机的颜色缓存标识发送到RenderPass, 表示用当前相机颜色缓存的内容进行渲染
+        // 初始化当前Pass, 将当前摄像机记录颜色缓存的RenderTexture标识发送到RenderPass
+        // 表示用当前相机颜色缓存的内容进行渲染
         zoomBlurPass.Setup(renderer.cameraColorTarget);
         // 将当前Pass添加到渲染队列中(renderer)
         renderer.EnqueuePass(zoomBlurPass);
